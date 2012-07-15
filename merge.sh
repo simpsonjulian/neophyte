3702c4a312e0cadab48db16b7c93d3837c1c358d
Julian Simpson <simpsonjulian@gmail.com>
2012-07-15 11:20:54 +0100

    this doesn't work but I am sharing with the group

--- merge.sh
+++ merge.sh
@@ -1,6 +1,14 @@
-for project in community advanced enterprise manual packaging; do 
+#!/bin/bash
+merge_branch() {
+  local project=$1
+  local branch=$2
   git remote add -f ${project} git://github.com/neo4j/${project}.git
-  git merge -s ours --no-commit ${project}/master
-  git read-tree --prefix=${project}/ -u ${project}/master
+  git merge -s ours --no-commit ${project}/${branch}
+  git read-tree --prefix=${project}/ -u ${project}/${branch}
   git commit -m "Subtree merged in ${project}"
+}
+for project in community advanced enterprise manual packaging; do 
+  for branch in 1.4-maint 1.5-maint 1.6-maint 1.7-maint 1.8-maint; do
+    merge_branch $project $branch
+  done
 done
