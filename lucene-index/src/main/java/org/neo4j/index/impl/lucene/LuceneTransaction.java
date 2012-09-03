simpsonjulian/neophyte

commit efc01d41dfbd9487c1d3e7735cf8ff45fd2b4234

Fixes an issue where recovery on a log which contained changes to an index followed by deletion of that index. During recovery an exception was thrown previously when trying to do the changes to the index as it didn't exist at that time.

lucene-index/src/main/java/org/neo4j/index/impl/lucene/LuceneTransaction.java
@@ -255,7 +255,22 @@ class LuceneTransaction extends XaTransaction
                 IndexIdentifier identifier = entry.getKey();
                 CommandList commandList = entry.getValue();
                 IndexType type = identifier == LuceneCommand.CreateIndexCommand.FAKE_IDENTIFIER
-                                 || !commandList.containsWrites() ? null : dataSource.getType( identifier );
+                                 || !commandList.containsWrites() ? null : dataSource.getType( identifier, isRecovered() );
+                
+                // This is for an issue where there are changes to and index which in a later
+                // transaction becomes deleted and crashes before the next rotation.
+                // The next recovery process will then recover that log and do those changes
+                // to the index, which at this point doesn't exist. So just don't do those
+                // changes as it will be deleted "later" anyway.
+                if ( type == null && isRecovered() )
+                {
+                    if ( commandList.isDeletion() )
+                        dataSource.removeExpectedFutureDeletion( identifier );
+                    else if ( commandList.containsWrites() )
+                        dataSource.addExpectedFutureDeletion( identifier );
+                    continue;
+                }
+                
                 CommitContext context = null;
                 try
                 {
@@ -461,6 +476,11 @@ class LuceneTransaction extends XaTransaction
         boolean containsWrites()
         {
             return containsWrites;
+        }
+        
+        boolean isDeletion()
+        {
+            return commands.size() == 1 && commands.get( 0 ) instanceof DeleteCommand;
         }
 
         void clear()

lucene-index/src/main/java/org/neo4j/index/impl/lucene/IndexTypeCache.java
@@ -37,13 +37,19 @@ class IndexTypeCache
         this.indexStore = indexStore;
     }
     
-    IndexType getIndexType( IndexIdentifier identifier )
+    IndexType getIndexType( IndexIdentifier identifier, boolean recovery )
     {
         Pair<Integer, IndexType> type = cache.get( identifier );
         Map<String, String> config = indexStore.get( identifier.entityType.getType(), identifier.indexName );
         if ( type != null && config.hashCode() == type.first() )
         {
             return type.other();
+        }
+        if ( config == null )
+        {
+            if ( recovery )
+                return null;
+            throw new IllegalArgumentException( "Unknown index " + identifier );
         }
         type = Pair.of( config.hashCode(), IndexType.getIndexType( identifier, config ) );
         cache.put( identifier, type );

lucene-index/src/main/java/org/neo4j/index/impl/lucene/LuceneDataSource.java
@@ -30,9 +30,11 @@ import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.lucene.analysis.Analyzer;
@@ -63,6 +65,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.PropertyContainer;
 import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.TransactionFailureException;
 import org.neo4j.graphdb.factory.GraphDatabaseSetting;
 import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.graphdb.index.Index;
@@ -162,6 +165,9 @@ public class LuceneDataSource extends LogBackedXaDataSource
     final Map<IndexIdentifier, LuceneIndex<? extends PropertyContainer>> indexes =
             new HashMap<IndexIdentifier, LuceneIndex<? extends PropertyContainer>>();
     private final DirectoryGetter directoryGetter;
+    
+    // Used for assertion after recovery has been completed.
+    private final Set<IndexIdentifier> expectedFutureRecoveryDeletions = new HashSet<IndexIdentifier>();
 
     /**
      * Constructs this data source.
@@ -235,9 +241,9 @@ public class LuceneDataSource extends LogBackedXaDataSource
         }
     }
 
-    IndexType getType( IndexIdentifier identifier )
+    IndexType getType( IndexIdentifier identifier, boolean recovery )
     {
-        return typeCache.getIndexType( identifier );
+        return typeCache.getIndexType( identifier, recovery );
     }
 
     private void cleanWriteLocks( String directory )
@@ -394,6 +400,15 @@ public class LuceneDataSource extends LogBackedXaDataSource
                 }
             }
             providerStore.flush();
+        }
+        
+        @Override
+        public void recoveryComplete()
+        {
+            if ( !expectedFutureRecoveryDeletions.isEmpty() )
+                throw new TransactionFailureException( "Recovery discovered transactions which couldn't " +
+                		"be applied due to a future index deletion, however some expected deletions " +
+                		"weren't encountered: " + expectedFutureRecoveryDeletions );
         }
 
         @Override
@@ -648,7 +663,7 @@ public class LuceneDataSource extends LogBackedXaDataSource
         {
             Directory dir = directoryGetter.getDirectory( baseStorePath, identifier ); //getDirectory( baseStorePath, identifier );
             directoryExists( dir );
-            IndexType type = getType( identifier );
+            IndexType type = getType( identifier, false );
             IndexWriterConfig writerConfig = new IndexWriterConfig( LUCENE_VERSION, type.analyzer );
             writerConfig.setIndexDeletionPolicy( new MultipleBackupDeletionPolicy() );
             Similarity similarity = type.getSimilarity();
@@ -909,5 +924,15 @@ public class LuceneDataSource extends LogBackedXaDataSource
         };
 
         abstract Directory getDirectory( String baseStorePath, IndexIdentifier identifier ) throws IOException;
+    }
+
+    void addExpectedFutureDeletion( IndexIdentifier identifier )
+    {
+        expectedFutureRecoveryDeletions.add( identifier );
+    }
+    
+    void removeExpectedFutureDeletion( IndexIdentifier identifier )
+    {
+        expectedFutureRecoveryDeletions.remove( identifier );
     }
 }

lucene-index/src/main/java/org/neo4j/index/impl/lucene/LuceneIndex.java
@@ -72,7 +72,7 @@ public abstract class LuceneIndex<T extends PropertyContainer> implements Index<
     {
         this.service = service;
         this.identifier = identifier;
-        this.type = service.dataSource().getType( identifier );
+        this.type = service.dataSource().getType( identifier, false );
     }
 
     LuceneXaConnection getConnection()

lucene-index/src/test/java/org/neo4j/index/impl/lucene/AddThenDeleteInAnotherTxAndQuit.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (c) 2002-2012 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.index.impl.lucene;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.index.Index;
+
+/**
+ * This class is used by {@link TestRecovery} so that a graph database can
+ * be shut down in a non-clean way after index add, then index delete.
+ */
+public class AddThenDeleteInAnotherTxAndQuit
+{
+    public static void main( String[] args )
+    {
+        GraphDatabaseService db = new GraphDatabaseFactory().newEmbeddedDatabase( args[0] );
+        Index<Node> index = db.index().forNodes( "index" );
+        Transaction tx = db.beginTx();
+        try
+        {
+            Node node = db.createNode();
+            index.add( node, "key", "value" );
+            tx.success();
+        }
+        finally
+        {
+            tx.finish();
+        }
+        
+        tx = db.beginTx();
+        try
+        {
+            index.delete();
+            tx.success();
+        }
+        finally
+        {
+            tx.finish();
+        }
+        
+        System.exit( 0 );
+    }
+}

lucene-index/src/test/java/org/neo4j/index/impl/lucene/TestRecovery.java
@@ -146,4 +146,21 @@ public class TestRecovery
                                                    new XaFactory( config, TxIdGenerator.DEFAULT, new PlaceboTm(), new DefaultLogBufferFactory(), fileSystemAbstraction, StringLogger.DEV_NULL, RecoveryVerifier.ALWAYS_VALID, LogPruneStrategies.NO_PRUNING ));
         ds.close();
     }
+
+    @Test
+    public void recoveryOnDeletedIndex() throws Exception
+    {
+        GraphDatabaseService db = newGraphDbService();
+        db.index().forNodes( "index" );
+        db.shutdown();
+
+        Process process = Runtime.getRuntime().exec( new String[]{
+            "java", "-cp", System.getProperty( "java.class.path" ),
+            AddThenDeleteInAnotherTxAndQuit.class.getName(), getDbPath()
+        } );
+        assertEquals( 0, new ProcessStreamHandler( process, true ).waitForResult() );
+        
+        new GraphDatabaseFactory().newEmbeddedDatabase( getDbPath() ).shutdown();
+        db.shutdown();
+    }
 }

commit 82f1810cb171492366968b3c5223af02a842d913

Fixes a problem introduced by efc01d41dfbd9487c1d3e7735cf8ff45fd2b4234 regarding recovering index creations

lucene-index/src/main/java/org/neo4j/index/impl/lucene/LuceneTransaction.java
@@ -265,10 +265,15 @@ class LuceneTransaction extends XaTransaction
                 if ( type == null && isRecovered() )
                 {
                     if ( commandList.isDeletion() )
+                    {
                         dataSource.removeExpectedFutureDeletion( identifier );
+                        continue;
+                    }
                     else if ( commandList.containsWrites() )
+                    {
                         dataSource.addExpectedFutureDeletion( identifier );
-                    continue;
+                        continue;
+                    }
                 }
                 
                 CommitContext context = null;